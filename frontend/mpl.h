93362ac14917f9ab692a357c6bbb7ff4c95d5ccc
David Hollman <dshollm@sandia.gov>
2018-05-17 09:49:02 -0700

    switched functions in frontend/mpl.h to use tinympl

--- frontend/mpl.h
+++ frontend/mpl.h
@@ -1,36 +1,27 @@
 #ifndef mpl_h_included
 #define mpl_h_included
 
-template < class T >                                                              
-class HasMemberType_Permissions
-{                                                                                 
- private:                                                                          
-  using Yes = char[2];                                                          
-  using  No = char[1];                                                          
+#include <tinympl/detection.hpp>
+#include <tinympl/reverse.hpp>
+#include <tinympl/tuple_as_sequence.hpp>
 
-  template < class U >                                                          
-  static Yes& test ( typename U::Permissions* );                                        
-  template < typename U >                                                       
-  static No& test ( U* );                                                      
+namespace detail {
 
-public:                                                                           
-  static constexpr bool RESULT = sizeof(test<T>(nullptr)) == sizeof(Yes); 
-};                                                                                
+template <typename T>
+using _has_member_type_Permissions_archetype = typename T::Permissions;
+
+} // end namespace detail
 
 template < class T >                                                              
 struct has_member_type_Permissions
-: public std::integral_constant<bool, HasMemberType_Permissions<T>::RESULT>            
-{ };  
+  : tinympl::is_detected<detail::_has_member_type_Permissions_archetype, T>
+{ };
 
-template <int N, class T, class... Args>
+template <int N, class... Args>
 struct reverse_tuple {
-  using type_t = typename reverse_tuple<N-1,Args...,T>::type_t;
+  using type_t = typename tinympl::reverse<std::tuple<Args...>>::type;
 };
 
-template <class T, class... Args>
-struct reverse_tuple<1, T, Args...> {
-  using type_t = std::tuple<Args...,T>;
-};
 
 #endif
 
